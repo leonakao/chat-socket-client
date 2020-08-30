0942033dc4bf661114798595118989c82ed4b17a
Leonardo Nakao <leonakao.dev@gmail.com>
2020-08-29 21:06:43 -0300

    feat: add basic authentication

--- src/services/ChatConnection.js
+++ src/services/ChatConnection.js
@@ -2,21 +2,22 @@ import io from 'socket.io-client';
 
 export default () => {
     const socket = io('http://localhost:3333', {
-        transportOptions: {
-            polling: {
-                extraHeaders: {
-                    'Authorization': 'abc'
-                }
-            }
-        }
+        query: {
+            Authorization: 'token'
+        },
+        forceNew: true
     });
 
     socket.on('connect', () => {
         console.log(`connected: ${socket.id}`);
     });
 
-    socket.on('disconnect', () => {
-        console.log('disconnected');
+    socket.on('disconnect', reason => {
+        console.log('disconnected: ' + reason);
+    });
+
+    socket.on('error', error => {
+        console.log('error: ', error);
     });
 
     return socket;
