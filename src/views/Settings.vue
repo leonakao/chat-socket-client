4df1124384b5a664f0467f74f47e8f44a28a8dd3
Leonardo Nakao <leonakao.dev@gmail.com>
2020-10-02 20:23:57 -0300

    feat: disable resturant accounts

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -38,6 +38,7 @@
                                             :key="setting.type"
                                             color="warning"
                                             :value="setting"
+                                            :disabled="setting.disabled"
                                         >
                                             {{ setting.name }}
                                         </v-btn>
@@ -66,22 +67,22 @@ export default {
     data: () => {
         return {
             settings: [
-                { name: 'User', page: 'UserView', users: [
+                { name: 'User', page: 'UserView', disabled: false, users: [
                     { name: 'User 17443', auth: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lciI6IjE3NDQzIiwiZ2VyYWRvIjoiMjAyMC0wOS0yNCAwMzowNjozOCJ9.M9I474ty2-sKx8S-hNAhLiaaVpwX_8c3OkXcr5Icf-U' },
                     { name: 'User 43149', auth: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lciI6IjQzMTQ5IiwiZ2VyYWRvIjoiMjAyMC0wOS0yNCAwMzowNjozOCJ9.76T54nBiD-HxufyL7IH__P_p47T9t_tWRaArbeYj6yE' },
                     { name: 'User 40003', auth: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lciI6IjQwMDAzIiwiZ2VyYWRvIjoiMjAyMC0wOS0yNCAwMzowNjozOCJ9.8Ha29Touq-DTPWi0l_qqu_o7-7_aT_NQC908y2xZLTg' },
                 ]},
-                { name: 'Restaurant', page: 'RestView', users: [
+                { name: 'Restaurant', page: 'RestView', disabled: true, users: [
                     { name: 'Restaurant 641', auth: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzaG9wIjoiNjQxIiwiZ2VyYWRvIjoiMjAyMC0wOS0yNCAwMzowNjozOCJ9.FF6EtT2hZ6HHIhA6_mGgTATmugxbjvA-HSHtv_5SLLQ' },
                     { name: 'Restaurant 154', auth: 'seyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzaG9wIjoiMTU0IiwiZ2VyYWRvIjoiMjAyMC0wOS0yNCAwMzowNjozOCJ9.YOFNYjepsgTiFhzC5y5Z775HtSfH0_0xV-eTJizGLow' },
                     { name: 'Restaurant 316', auth: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzaG9wIjoiMzE2IiwiZ2VyYWRvIjoiMjAyMC0wOS0yNCAwMzowNjozOCJ9.EMOSt4B9oLQ8HXRJAgrYq3iDFRmQ0TVFRaQoqUBzND0' },
                 ]},
-                { name: 'Delivery', page: 'MotoView', users: [
+                { name: 'Delivery', page: 'MotoView', disabled: false, users: [
                     { name: 'Delivery 618', auth: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkZWxpdmVyeSI6IjYxOCIsImdlcmFkbyI6IjIwMjAtMDktMjQgMDM6MDY6MzgifQ.zq4-x2KMQW5R26rGmL84fVVaNCKT-z3-gRvIOgCe8CI' },
                     { name: 'Delivery 154', auth: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkZWxpdmVyeSI6IjE1NCIsImdlcmFkbyI6IjIwMjAtMDktMjQgMDM6MDY6MzgifQ.FT5oP66XePkarnNqg8FMuEGrUmvFYoSlWSwfX-gZVGY' },
                     { name: 'Delivery 344', auth: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkZWxpdmVyeSI6IjM0NCIsImdlcmFkbyI6IjIwMjAtMDktMjQgMDM6MDY6MzgifQ.E01EgXK1S-3w5T4ti6EBW66q_h8cEY5ln9EV7Dpi5KM' },
                 ]},
-                { name: 'Support', page: 'SuptView', users: [
+                { name: 'Support', page: 'SuptView', disabled: false, users: [
                     { name: 'Support 300', auth: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdXBwb3J0IjoiMzAwIiwiZ2VyYWRvIjoiMjAyMC0wOS0yNCAwMzowNjozOCJ9.uUNOHfz0cbsEbwqhd2RVktpP1bdISx1Ee9XOUG5amow' },
                     { name: 'Support 301', auth: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdXBwb3J0IjoiMzAxIiwiZ2VyYWRvIjoiMjAyMC0wOS0yNCAwMzowNjozOCJ9.T3n5HUTCJ6M41xETqhJhlFaOIPCJgYjDmJlcKhpKfqY' },
                     { name: 'Support 302', auth: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdXBwb3J0IjoiMzAyIiwiZ2VyYWRvIjoiMjAyMC0wOS0yNCAwMzowNjozOCJ9._VQxPhJg1UkxqdQKmp4UoBmpg3ZsAwdpc4FJWLejT7U' },
